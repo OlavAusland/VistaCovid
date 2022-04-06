a5dcce44c2a1eae36ee58d9639b50e77e7548c75
Olav Ausland Onstad <olavao@uia.no>
2022-04-06 20:43:31 +0200

    Added Tab Navigation

---
+++ components/Profile.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+import { useState } from 'react';
+
+export function ProfileView() 
+{
+    return (
+        <View>
+            <Text>Profile</Text>
+        </View>
+    );
+}
---
+++ components/Register.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+import { useState } from 'react';
+
+export function RegisterView()
+{
+    return(
+        <View>
+            <Text>Register</Text>
+        </View>
+    );
+}
