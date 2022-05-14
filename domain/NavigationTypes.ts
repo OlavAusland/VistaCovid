6def934eb7f3d46908cbed5334dc804526aa9995
Olav Ausland Onstad <olavao@uia.no>
2022-05-14 17:59:23 +0200

    Fixed Merge

--- domain/NavigationTypes.ts
+++ domain/NavigationTypes.ts
@@ -1,15 +1,15 @@
 export type StackParameters = {
-    Register : object | undefined,
-    Profile : object | undefined,
-    Login : object | undefined,
-    Home : object | undefined,
-    Admin : object | undefined,
-    VistaCovid : object | undefined,
+    Register : object,
+    Profile : object,
+    Login : object,
+    Home : object,
+    Admin : object,
+    VistaCovid : object,
     AddRoom : object | undefined,
     DeleteRoom : object | undefined,
     ManageRoom : object | undefined,
     ManageRoles : object | undefined,
-    Room: object | undefined
+    Room: {roomId: string}
 }
 
 export type TabParameters = {
