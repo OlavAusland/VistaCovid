925f7dc5c2cb5b74214ca3dd3fc1fad34d59419b
Olav Ausland Onstad <olavao@uia.no>
2022-05-19 04:55:04 +0200

    Fix

--- components/room/GraphView.tsx
+++ components/room/GraphView.tsx
@@ -10,17 +10,17 @@ export type GraphViewProps = {
 export const GraphView = (props: GraphViewProps) => {
     return (
         <ScrollView testID='graphView' contentContainerStyle={roomStyle.body}>
-            {props.room.heartRate.length > 0 &&
+            {props.room.heartRate !== undefined && props.room.heartRate.length > 0 &&
                 <View style={roomStyle.graphContainer}>
                     <LineGraph color={'#eb4034'} data={props.room?.heartRate} name={'Heart Rate'}/>
                 </View>
             }
-            {props.room.respirationRate.length > 0 &&
+            {props.room.respirationRate !== undefined && props.room.respirationRate.length > 0 &&
                 <View style={roomStyle.graphContainer}>
                     <LineGraph color={'#edb855'} data={props.room?.respirationRate} name={'Respiration Rate'}/>
                 </View>
             }
-            {props.room.oxygenLevel.length > 0 &&
+            {props.room.oxygenLevel !== undefined && props.room.oxygenLevel.length > 0 &&
                 <View style={roomStyle.graphContainer}>
                     <LineGraph color={'#6ed7e0'} data={props.room?.oxygenLevel}  name={'Oxygen Level'}/>
                 </View>
